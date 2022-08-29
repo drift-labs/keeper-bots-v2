0efe68b9568419a4eb5a880b99d4cf9483381b0b
wphan <william@drift.trade>
2022-08-28 23:34:07 -0400

    refactor filler code to reduce starvation

--- src/bots/filler.ts
+++ src/bots/filler.ts
@@ -9,21 +9,24 @@ import {
 	MakerInfo,
 	isFillableByVAMM,
 } from '@drift-labs/sdk';
+import { promiseTimeout } from '@drift-labs/sdk/lib/util/promiseTimeout';
 
 import { SendTransactionError } from '@solana/web3.js';
 
 import { getErrorCode, getErrorMessage } from '../error';
 import { logger } from '../logger';
-import { DLOB } from '../dlob/DLOB';
+import { DLOB, NodeToFill } from '../dlob/DLOB';
 import { UserMap } from '../userMap';
 import { UserStatsMap } from '../userStatsMap';
 import { Bot } from '../types';
 import { Metrics } from '../metrics';
 
+const FILL_ORDER_BACKOFF = 5000;
+
 export class FillerBot implements Bot {
 	public readonly name: string;
 	public readonly dryRun: boolean;
-	public readonly defaultIntervalMs: number = 1000;
+	public readonly defaultIntervalMs: number = 5000;
 
 	private clearingHouse: ClearingHouse;
 	private slotSubscriber: SlotSubscriber;
@@ -81,8 +84,8 @@ export class FillerBot implements Bot {
 	}
 
 	public async startIntervalLoop(intervalMs: number): Promise<void> {
-		this.tryFill();
-		const intervalId = setInterval(this.tryFill.bind(this), intervalMs);
+		await this.tryFill();
+		const intervalId = setInterval(await this.tryFill.bind(this), intervalMs);
 		this.intervalIds.push(intervalId);
 
 		logger.info(`${this.name} Bot started!`);
@@ -96,7 +99,7 @@ export class FillerBot implements Bot {
 				record as OrderRecord,
 				this.userMap
 			);
-			this.tryFill();
+			// this.tryFill();
 		}
 	}
 
@@ -104,186 +107,222 @@ export class FillerBot implements Bot {
 		return this.dlob;
 	}
 
-	private async tryFillForMarket(market: MarketAccount) {
+	private getFillableNodesForMarket(market: MarketAccount): Array<NodeToFill> {
 		const marketIndex = market.marketIndex;
+		const oraclePriceData =
+			this.clearingHouse.getOracleDataForMarket(marketIndex);
+		const oracleIsValid = isOracleValid(
+			market.amm,
+			oraclePriceData,
+			this.clearingHouse.getStateAccount().oracleGuardRails,
+			this.slotSubscriber.getSlot()
+		);
+
+		const vAsk = calculateAskPrice(market, oraclePriceData);
+		const vBid = calculateBidPrice(market, oraclePriceData);
+
+		return this.dlob.findNodesToFill(
+			marketIndex,
+			vBid,
+			vAsk,
+			this.slotSubscriber.getSlot(),
+			oracleIsValid ? oraclePriceData : undefined
+		);
+	}
+
+	private async tryFillWithTimeout(nodeToFill: NodeToFill) {
+		if (nodeToFill.node.haveFilled) {
+			return;
+		}
+		if (
+			nodeToFill.node.lastFillAttempt &&
+			nodeToFill.node.lastFillAttempt.getTime() + FILL_ORDER_BACKOFF >
+				new Date().getTime()
+		) {
+			logger.error(
+				`${
+					this.name
+				} backingoff for order (account: ${nodeToFill.node.userAccount.toString()}) order ${nodeToFill.node.order.orderId.toString()} on mktIdx: ${nodeToFill.node.market.marketIndex.toString()}`
+			);
+			return;
+		}
+
+		const marketIndex = nodeToFill.node.market.marketIndex;
+		const oraclePriceData =
+			this.clearingHouse.getOracleDataForMarket(marketIndex);
 
 		if (
-			Atomics.compareExchange(
-				this.perMarketMutexFills,
-				marketIndex.toNumber(),
-				0,
-				1
-			) === 1
+			!nodeToFill.makerNode &&
+			!isFillableByVAMM(
+				nodeToFill.node.order,
+				nodeToFill.node.market,
+				oraclePriceData,
+				this.slotSubscriber.getSlot(),
+				this.clearingHouse.getStateAccount().maxAuctionDuration
+			)
 		) {
 			return;
 		}
 
-		try {
-			const oraclePriceData =
-				this.clearingHouse.getOracleDataForMarket(marketIndex);
-			const oracleIsValid = isOracleValid(
-				market.amm,
-				oraclePriceData,
-				this.clearingHouse.getStateAccount().oracleGuardRails,
-				this.slotSubscriber.getSlot()
-			);
+		nodeToFill.node.haveFilled = true;
 
-			const vAsk = calculateAskPrice(market, oraclePriceData);
-			const vBid = calculateBidPrice(market, oraclePriceData);
+		// logger.info(
+		// 	`${
+		// 		this.name
+		// 	} trying to fill (account: ${nodeToFill.node.userAccount.toString()}) order ${nodeToFill.node.order.orderId.toString()} on mktIdx: ${marketIndex.toString()}`
+		// );
 
-			const nodesToFill = this.dlob.findNodesToFill(
-				marketIndex,
-				vBid,
-				vAsk,
-				this.slotSubscriber.getSlot(),
-				oracleIsValid ? oraclePriceData : undefined
-			);
-			this.metrics?.recordFillableOrdersSeen(
-				marketIndex.toNumber(),
-				nodesToFill.length
-			);
+		if (nodeToFill.makerNode) {
+			`${
+				this.name
+			} including maker: ${nodeToFill.makerNode.userAccount.toString()}) with order ${nodeToFill.makerNode.order.orderId.toString()}`;
+		}
 
-			for (const nodeToFill of nodesToFill) {
-				if (nodeToFill.node.haveFilled) {
-					continue;
-				}
-				if (
-					nodeToFill.node.lastFillAttempt &&
-					nodeToFill.node.lastFillAttempt.getTime() + 10000 >
-						new Date().getTime()
-				) {
-					continue;
-				}
+		let makerInfo: MakerInfo | undefined;
+		if (nodeToFill.makerNode) {
+			const makerAuthority = (
+				await this.userMap.mustGet(nodeToFill.makerNode.userAccount.toString())
+			).getUserAccount().authority;
+			const makerUserStats = (
+				await this.userStatsMap.mustGet(makerAuthority.toString())
+			).userStatsAccountPublicKey;
+			makerInfo = {
+				maker: nodeToFill.makerNode.userAccount,
+				order: nodeToFill.makerNode.order,
+				makerStats: makerUserStats,
+			};
+		}
 
-				if (
-					!nodeToFill.makerNode &&
-					!isFillableByVAMM(
+		const user = await this.userMap.mustGet(
+			nodeToFill.node.userAccount.toString()
+		);
+
+		const referrerInfo = (
+			await this.userStatsMap.mustGet(
+				user.getUserAccount().authority.toString()
+			)
+		).getReferrerInfo();
+
+		if (this.dryRun) {
+			logger.info(`${this.name} dry run, not filling`);
+			return;
+		}
+
+		const reqStart = Date.now();
+		this.metrics?.recordRpcRequests('fillOrder', this.name);
+		const txSig: any | null = await promiseTimeout(
+			this.clearingHouse.fillOrder(
+				nodeToFill.node.userAccount,
+				user.getUserAccount(),
+				nodeToFill.node.order,
+				makerInfo,
+				referrerInfo
+			),
+			10 * 1000
+		)
+			.catch((error) => {
+				nodeToFill.node.haveFilled = false;
+				nodeToFill.node.lastFillAttempt = new Date();
+
+				const errorCode = getErrorCode(error);
+				this.metrics?.recordErrorCode(
+					errorCode,
+					this.clearingHouse.provider.wallet.publicKey,
+					this.name
+				);
+
+				const errorMessage = getErrorMessage(error as SendTransactionError);
+
+				if (errorMessage === 'OrderDoesNotExist') {
+					this.dlob.remove(
 						nodeToFill.node.order,
-						market,
-						oraclePriceData,
-						this.slotSubscriber.getSlot(),
-						this.clearingHouse.getStateAccount().maxAuctionDuration
-					)
-				) {
-					continue;
-				}
-
-				nodeToFill.node.haveFilled = true;
-
-				logger.info(
-					`${
-						this.name
-					} trying to fill (account: ${nodeToFill.node.userAccount.toString()}) order ${nodeToFill.node.order.orderId.toString()}`
-				);
-
-				if (nodeToFill.makerNode) {
-					`${
-						this.name
-					} including maker: ${nodeToFill.makerNode.userAccount.toString()}) with order ${nodeToFill.makerNode.order.orderId.toString()}`;
-				}
-
-				let makerInfo: MakerInfo | undefined;
-				if (nodeToFill.makerNode) {
-					const makerAuthority = (
-						await this.userMap.mustGet(
-							nodeToFill.makerNode.userAccount.toString()
-						)
-					).getUserAccount().authority;
-					const makerUserStats = (
-						await this.userStatsMap.mustGet(makerAuthority.toString())
-					).userStatsAccountPublicKey;
-					makerInfo = {
-						maker: nodeToFill.makerNode.userAccount,
-						order: nodeToFill.makerNode.order,
-						makerStats: makerUserStats,
-					};
-				}
-
-				const user = await this.userMap.mustGet(
-					nodeToFill.node.userAccount.toString()
-				);
-
-				const referrerInfo = (
-					await this.userStatsMap.mustGet(
-						user.getUserAccount().authority.toString()
-					)
-				).getReferrerInfo();
-
-				if (this.dryRun) {
-					logger.info(`${this.name} dry run, not filling`);
-					continue;
-				}
-
-				this.clearingHouse
-					.fillOrder(
 						nodeToFill.node.userAccount,
-						user.getUserAccount(),
-						nodeToFill.node.order,
-						makerInfo,
-						referrerInfo
-					)
-					.then((txSig) => {
-						this.metrics?.recordFilledOrder(
-							this.clearingHouse.provider.wallet.publicKey,
-							this.name
-						);
-						logger.info(
-							`${
-								this.name
-							} Filled user (account: ${nodeToFill.node.userAccount.toString()}) order: ${nodeToFill.node.order.orderId.toString()}, Tx: ${txSig}`
-						);
-					})
-					.catch((error) => {
-						nodeToFill.node.haveFilled = false;
-						nodeToFill.node.lastFillAttempt = new Date();
-
-						// If we get an error that order does not exist, assume its been filled by somebody else and we
-						// have received the history record yet
-						// TODO this might not hold if events arrive out of order
-						const errorCode = getErrorCode(error);
-						this.metrics?.recordErrorCode(
-							errorCode,
-							this.clearingHouse.provider.wallet.publicKey,
-							this.name
-						);
-
-						const errorMessage = getErrorMessage(error as SendTransactionError);
-
-						if (errorMessage === 'OrderDoesNotExist') {
-							this.dlob.remove(
-								nodeToFill.node.order,
-								nodeToFill.node.userAccount,
-								() => {
-									logger.error(
-										`Order ${nodeToFill.node.order.orderId.toString()} not found when trying to fill. Removing from order list`
-									);
-								}
+						() => {
+							logger.error(
+								`Order ${nodeToFill.node.order.orderId.toString()} not found when trying to fill. Removing from order list`
 							);
-							// dlob.printTopOfOrderLists(this.clearingHouse, nodeToFill.node.order.marketIndex);
 						}
-						logger.error(
-							`Error (${errorCode}) filling user (account: ${nodeToFill.node.userAccount.toString()}) order: ${nodeToFill.node.order.orderId.toString()}`
-						);
-						// console.error(error);
-					});
-			}
-		} catch (e) {
+					);
+				}
+				logger.error(
+					`Error (${errorCode}) filling user (account: ${nodeToFill.node.userAccount.toString()}) order: ${nodeToFill.node.order.orderId.toString()}, mktIdx: ${marketIndex.toNumber()}`
+				);
+			})
+			.finally(() => {
+				const duration = Date.now() - reqStart;
+				this.metrics?.recordRpcDuration(
+					this.clearingHouse.connection.rpcEndpoint,
+					'fillOrder',
+					duration,
+					txSig === null,
+					this.name
+				);
+			});
+
+		if (txSig === null) {
 			logger.error(
+				`Timeout filling order mktIdx: ${marketIndex.toNumber()} (account: ${nodeToFill.node.userAccount.toString()}) order: ${nodeToFill.node.order.orderId.toString()}`
+			);
+			return;
+		} else if (txSig === undefined) {
+			// this is the case when it hits the catch block above
+			return;
+		} else {
+			this.metrics?.recordFilledOrder(
+				this.clearingHouse.provider.wallet.publicKey,
+				this.name
+			);
+			logger.info(
 				`${
 					this.name
-				} Unexpected error for market ${marketIndex.toString()} during fills: ${JSON.stringify(
-					e
-				)}`
+				} Filled user (account: ${nodeToFill.node.userAccount.toString()}) order: ${nodeToFill.node.order.orderId.toString()}, Tx: ${txSig}`
 			);
-			console.error(e);
-		} finally {
-			Atomics.store(this.perMarketMutexFills, marketIndex.toNumber(), 0);
 		}
 	}
 
-	private tryFill() {
-		for (const marketAccount of this.clearingHouse.getMarketAccounts()) {
-			this.tryFillForMarket(marketAccount);
+	private async tryFill() {
+		const startTime = Date.now();
+		if (Atomics.compareExchange(this.perMarketMutexFills, 0, 0, 1) === 1) {
+			logger.info(`${this.name} tryFill is bizzy`);
+			return false;
+		}
+
+		// 1) get all fillable nodes
+		const markets = this.clearingHouse.getMarketAccounts();
+		const fillableNodes: Array<NodeToFill> = [];
+		for (const market of markets) {
+			fillableNodes.push(...this.getFillableNodesForMarket(market));
+		}
+
+		// 2) fill each node
+		logger.info(`Fillable nodes: ${fillableNodes.length}`);
+		const fillResult = await promiseTimeout(
+			Promise.all(
+				fillableNodes.map((nodeToFill) => {
+					if (nodeToFill.node.isVammNode()) {
+						logger.info('vamm node');
+						return Promise.resolve();
+					}
+					logger.info(
+						`fillable node (account: ${nodeToFill.node.userAccount.toString()}) order ${nodeToFill.node.order.orderId.toString()} on mktIdx: ${nodeToFill.node.market.marketIndex.toString()}`
+					);
+
+					this.tryFillWithTimeout(nodeToFill);
+				})
+			),
+			10000
+		);
+
+		if (Atomics.compareExchange(this.perMarketMutexFills, 0, 1, 0) !== 1) {
+			logger.error(`${this.name} tryFill had incorrect mutex value`);
+			return;
+		}
+		if (fillResult === null) {
+			logger.error(`Timeout tryFill, took ${Date.now() - startTime}ms`);
+		} else {
+			logger.info(
+				`${this.name} finished tryFill market took ${Date.now() - startTime}ms`
+			);
 		}
 	}
 }
