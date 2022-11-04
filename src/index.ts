49ac9ce870cdbd61b065ce1aac641ffa8a6dfe3a
wphan <william@drift.trade>
2022-11-04 17:41:46 +0000

    bump sdk, fix missing ATA

--- src/index.ts
+++ src/index.ts
@@ -272,13 +272,18 @@ const runBot = async () => {
 	);
 	logger.info(`Wallet pubkey: ${wallet.publicKey.toBase58()}`);
 	logger.info(` . SOL balance: ${lamportsBalance / 10 ** 9}`);
-	const tokenAccount = await getOrCreateAssociatedTokenAccount(
-		connection,
-		new PublicKey(constants.devnet.USDCMint),
-		wallet
-	);
-	const usdcBalance = await connection.getTokenAccountBalance(tokenAccount);
-	logger.info(` . USDC balance: ${usdcBalance.value.uiAmount}`);
+
+	try {
+		const tokenAccount = await getOrCreateAssociatedTokenAccount(
+			connection,
+			new PublicKey(constants.devnet.USDCMint),
+			wallet
+		);
+		const usdcBalance = await connection.getTokenAccountBalance(tokenAccount);
+		logger.info(` . USDC balance: ${usdcBalance.value.uiAmount}`);
+	} catch (e) {
+		logger.info(`Failed to load USDC token account: ${e}`);
+	}
 
 	await clearingHouse.subscribe();
 	clearingHouse.eventEmitter.on('error', (e) => {
