9dba7357d8fd016355381da3edb23529d45bdf61
Ricardo J. Mendez <ricardo@numergent.com>
2023-10-25 11:09:43 +0200

    Removed unused imports from uncrossArbBot

--- src/bots/uncrossArbBot.ts
+++ src/bots/uncrossArbBot.ts
@@ -9,9 +9,7 @@ import {
 	DLOBSubscriber,
 	UserMap,
 	SlotSubscriber,
-	UserStatsMap,
 	MakerInfo,
-	PerpMarkets,
 	getUserStatsAccountPublicKey,
 } from '@drift-labs/sdk';
 import { Mutex, tryAcquire, E_ALREADY_LOCKED } from 'async-mutex';
@@ -20,7 +18,6 @@ import { Bot } from '../types';
 import {
 	getBestLimitAskExcludePubKey,
 	getBestLimitBidExcludePubKey,
-	sleepMs,
 } from '../utils';
 import { JitProxyClient } from '@drift-labs/jit-proxy/lib';
 import dotenv = require('dotenv');
@@ -29,7 +26,6 @@ dotenv.config();
 import { BaseBotConfig } from 'src/config';
 import {
 	AddressLookupTableAccount,
-	ComputeBudgetInstruction,
 	ComputeBudgetProgram,
 } from '@solana/web3.js';
 
