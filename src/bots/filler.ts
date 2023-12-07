aa1072d847efd19b98b74096820aa7f16c66836f
wphan <william@drift.trade>
2023-12-07 14:03:56 -0800

    filler ignore 6023

--- src/bots/filler.ts
+++ src/bots/filler.ts
@@ -112,6 +112,7 @@ const errorCodesToSuppress = [
 	6087, // 0x17c7 Error Number: 6087. Error Message: SpotMarketNotFound.
 	6239, // 0x185F Error Number: 6239. Error Message: RevertFill.
 	6003, // 0x1773 Error Number: 6003. Error Message: Insufficient collateral.
+	6023, // 0x1787 Error Number: 6023. Error Message: PriceBandsBreached.
 
 	6111, // Error Message: OrderNotTriggerable.
 	6112, // Error Message: OrderDidNotSatisfyTriggerCondition.
