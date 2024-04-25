d4c3e78bd37d8b46add182d527f36ea1e86e36e2
wphan <william@drift.trade>
2024-04-25 16:12:26 -0700

    fillers: use buffer when checking for expired orders (#182)

--- src/experimental-bots/filler/fillerMultithreaded.ts
+++ src/experimental-bots/filler/fillerMultithreaded.ts
@@ -1259,7 +1259,7 @@ export class FillerMultithreaded {
 		const oraclePriceData =
 			this.driftClient.getOracleDataForPerpMarket(marketIndex);
 
-		if (isOrderExpired(nodeToFill.node.order, Date.now() / 1000)) {
+		if (isOrderExpired(nodeToFill.node.order, Date.now() / 1000, true)) {
 			if (isOneOfVariant(nodeToFill.node.order.orderType, ['limit'])) {
 				// do not try to fill (expire) limit orders b/c they will auto expire when filled against
 				// or the user places a new order
@@ -2092,12 +2092,15 @@ export class FillerMultithreaded {
 				if (filledNode) {
 					const isExpired = isOrderExpired(
 						filledNode.node.order!,
-						Date.now() / 1000
+						Date.now() / 1000,
+						true
 					);
 					logger.error(
 						`assoc node (ixIdx: ${ixIdx}): ${filledNode.node.userAccount!.toString()}, ${
 							filledNode.node.order!.orderId
-						}; does not exist (filled by someone else); ${log}, expired: ${isExpired}`
+						}; does not exist (filled by someone else); ${log}, expired: ${isExpired}, orderTs: ${
+							filledNode.node.order!.maxTs
+						}, now: ${Date.now() / 1000}`
 					);
 					if (isExpired) {
 						const sig = getNodeToFillSignature(filledNode);
