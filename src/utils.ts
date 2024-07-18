54a4a7edba1c19a055636678ddf71a0e337c1d09
Nour Alharithi <nour.alharithi@gmail.com>
2024-07-18 16:07:44 -0700

    remove pyth ixs if fill is too big

--- src/utils.ts
+++ src/utils.ts
@@ -41,6 +41,7 @@ import {
 	PerpMarketConfig,
 	PerpMarkets,
 	SpotMarkets,
+	DRIFT_ORACLE_RECEIVER_ID,
 } from '@drift-labs/sdk';
 import {
 	NATIVE_MINT,
@@ -1303,4 +1304,82 @@ export async function initializeSpotFulfillmentAccounts(
 		serumSubscribers,
 		phoenixSubscribers,
 	};
+}
+
+export function removePythIxs(
+	ixs: TransactionInstruction[],
+	receiverPublicKeyStr: string = DRIFT_ORACLE_RECEIVER_ID
+): TransactionInstruction[] {
+	return ixs.filter(
+		(ix) =>
+			!ix.keys
+				.map((meta) => meta.pubkey.toString())
+				.includes(receiverPublicKeyStr)
+	);
+}
+
+export function getSizeOfTransaction(
+	instructions: TransactionInstruction[],
+	versionedTransaction = true,
+	addressLookupTables: AddressLookupTableAccount[] = []
+): number {
+	const programs = new Set<string>();
+	const signers = new Set<string>();
+	let accounts = new Set<string>();
+
+	instructions.map((ix) => {
+		programs.add(ix.programId.toBase58());
+		accounts.add(ix.programId.toBase58());
+		ix.keys.map((key) => {
+			if (key.isSigner) {
+				signers.add(key.pubkey.toBase58());
+			}
+			accounts.add(key.pubkey.toBase58());
+		});
+	});
+
+	const instruction_sizes: number = instructions
+		.map(
+			(ix) =>
+				1 +
+				getSizeOfCompressedU16(ix.keys.length) +
+				ix.keys.length +
+				getSizeOfCompressedU16(ix.data.length) +
+				ix.data.length
+		)
+		.reduce((a, b) => a + b, 0);
+
+	let numberOfAddressLookups = 0;
+	if (addressLookupTables.length > 0) {
+		const lookupTableAddresses = addressLookupTables
+			.map((addressLookupTable) =>
+				addressLookupTable.state.addresses.map((address) => address.toBase58())
+			)
+			.flat();
+		const totalNumberOfAccounts = accounts.size;
+		accounts = new Set(
+			[...accounts].filter((account) => !lookupTableAddresses.includes(account))
+		);
+		accounts = new Set([...accounts, ...programs, ...signers]);
+		numberOfAddressLookups = totalNumberOfAccounts - accounts.size;
+	}
+
+	return (
+		getSizeOfCompressedU16(signers.size) +
+		signers.size * 64 + // array of signatures
+		3 +
+		getSizeOfCompressedU16(accounts.size) +
+		32 * accounts.size + // array of account addresses
+		32 + // recent blockhash
+		getSizeOfCompressedU16(instructions.length) +
+		instruction_sizes + // array of instructions
+		(versionedTransaction ? 1 + getSizeOfCompressedU16(0) : 0) +
+		(versionedTransaction ? 32 * addressLookupTables.length : 0) +
+		(versionedTransaction && addressLookupTables.length > 0 ? 2 : 0) +
+		numberOfAddressLookups
+	);
+}
+
+function getSizeOfCompressedU16(n: number) {
+	return 1 + Number(n >= 128) + Number(n >= 16384);
 }
