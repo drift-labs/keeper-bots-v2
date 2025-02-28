27a02039d0fa2a0104969e50c060ecb5bb7c4156
Nour Alharithi <nour.alharithi@gmail.com>
2025-02-28 13:03:21 -0800

    only turn auctionsubsriberignore config if fastlaneordersu scriber is not undefined

--- src/index.ts
+++ src/index.ts
@@ -634,7 +634,7 @@ const runBot = async () => {
 			jitProxyClient,
 			fastlaneOrderSubscriber,
 			slotSubscriber,
-			auctionSubscriberIgnoresFastlaneOrders: true,
+			auctionSubscriberIgnoresFastlaneOrders: !!fastlaneOrderSubscriber,
 		});
 		await jitter.subscribe();
 
