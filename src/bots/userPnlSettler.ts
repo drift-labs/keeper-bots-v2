1f75b6c9d83b006e72f14e5491122e9e93493681
Nick Caradonna <lowkeynicc@gmail.com>
2025-07-28 15:29:43 -0400

    Revert "add debug logs to pnl settler"
    This reverts commit e954ca29979e617657ff7d97a1de9525f330696a.

--- src/bots/userPnlSettler.ts
+++ src/bots/userPnlSettler.ts
@@ -30,7 +30,7 @@ import {
 import { Mutex } from 'async-mutex';
 
 import { getErrorCode } from '../error';
-import { logger, setLogLevel } from '../logger';
+import { logger } from '../logger';
 import { Bot } from '../types';
 import { webhookMessage } from '../webhook';
 import { GlobalConfig, UserPnlSettlerConfig } from '../config';
@@ -116,7 +116,6 @@ export class UserPnlSettlerBot implements Bot {
 
 	public async init() {
 		logger.info(`${this.name} initing`);
-		setLogLevel('debug');
 
 		const driftMarkets: DriftMarketInfo[] = [];
 		for (const perpMarket of this.driftClient.getPerpMarketAccounts()) {
