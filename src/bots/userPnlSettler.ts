2a1543265868c916b74f23b2a94e97c558bdc49c
Nick Caradonna <lowkeynicc@gmail.com>
2025-07-28 17:24:25 -0400

    use abs()

--- src/bots/userPnlSettler.ts
+++ src/bots/userPnlSettler.ts
@@ -372,7 +372,7 @@ export class UserPnlSettlerBot implements Bot {
 					if (
 						(userUnsettledPnl.eq(ZERO) &&
 							settleePositionWithLp.lpShares.eq(ZERO)) ||
-						(!userUnsettledPnl.gte(this.minPnlToSettle) &&
+						(!userUnsettledPnl.abs().gte(this.minPnlToSettle) &&
 							!settleePositionWithLp.baseAssetAmount.eq(ZERO) &&
 							!isUsdcBorrow &&
 							settleePositionWithLp.lpShares.eq(ZERO))
